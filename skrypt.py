bf4fd8641248cfde66d9b85deadc557850e58666
Krupski Grzegorz <krupskig@gmail.com>
2016-01-06 18:41:37 +0100

    Skrypt będzie tworzył komendę do wykonania
    Będzie trzeba tylko printa zamienić na subprocess

--- skrypt.py
+++ skrypt.py
@@ -4,13 +4,15 @@ import psycopg2.extras
 import sys
 
 con = None
+uwykonana = False
 try:
     con = psycopg2.connect(database='psw',user='postgres',password='1234')
-    cur = con.cursor()
-    cur.execute('SELECT * FROM psw_commands WHERE ')
-    rows = cur.fetchall()
+    cursor = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute('SELECT * FROM psw_commands WHERE wykonana=%(wykonana)s', {'wykonana':uwykonana})
+    rows = cursor.fetchall()
     for row in rows:
-        print (row)
+        
+        print ("%s %s %s %s %s" % (row['id'], row['ip'], row['system'], row['ram'],row['quote']))
 
 except psycopg2.DatabaseError as e:
     print ('Error %s' %e)
